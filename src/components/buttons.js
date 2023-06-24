ecf9792bdb0fc0d4a78d329a0bc2cff09d4418ab
opeyemibioku <opeyemibioku@gmail.com>
2023-06-24 11:40:26 +0100

    updated landing page content

--- src/components/buttons.js
+++ src/components/buttons.js
@@ -2,8 +2,9 @@ export const CreativesLink = () => {
   return (
     <>
       <a
-        href=""
+        href="https://forms.gle/dVBbG4bWPPTNzfhc6"
         className="text-decoration-underline CreativesLink"
+        target="_blank"
         type="button"
       >
         Creatives <i className="bi bi-arrow-right"></i>
@@ -14,7 +15,11 @@ export const CreativesLink = () => {
 export const AuthorsLink = () => {
   return (
     <>
-      <a href="" className="text-decoration-underline AuthorsLink">
+      <a
+        href="https://docs.google.com/forms/d/1y3jtbbVYLJ9V9dldLTf5fQrywNclBzeUJ-lgwheMsGg/edit?usp=sharing_eip_se_dm&ts=6496c410"
+        className="text-decoration-underline AuthorsLink"
+        target="_blank"
+      >
         Authors <i className="bi bi-arrow-right"></i>
       </a>
     </>
@@ -23,11 +28,36 @@ export const AuthorsLink = () => {
 export const WorkWithUsBtn = () => {
   return (
     <>
-      <button type="button" class="btn WorkWithUsBtn btnText mx-1">
-        <a href="" target="_blank" className="text-white text-decoration-none">
+      <button type="button" class="WorkWithUsBtn btnText mx-1">
+        <b>
+          <a
+            href="https://forms.gle/mWnkHhN5qmJ1DVJK6"
+            target="_blank"
+            className="text-white text-decoration-none"
+          >
+            {" "}
+            Work With Us
+          </a>
+        </b>
+      </button>
+    </>
+  );
+};
+export const ChallengeBtn = () => {
+  return (
+    <>
+      <button type="button" class="WorkWithUsBtn btnText mx-1">
+        <b>
           {" "}
-          Work With Us
-        </a>
+          <a
+            href="https://forms.gle/dRdRsVyWfQa4Cks38"
+            target="_blank"
+            className="text-white text-decoration-none"
+          >
+            {" "}
+            45 Day Challenge
+          </a>
+        </b>
       </button>
     </>
   );
--- src/pages/home.js
+++ src/pages/home.js
@@ -4,6 +4,7 @@ import {
   WorkWithUsBtn,
   CreativesLink,
   AuthorsLink,
+  ChallengeBtn,
 } from "../components/buttons";
 
 export const SimplePage = () => {
@@ -13,22 +14,23 @@ export const SimplePage = () => {
       <div className="row align-items-center justify-content-center px-4">
         <div className="col-lg-6 col-md-7 col-sm-12 pt-3">
           <h2 className=" d-none d-lg-block d-md-none d-sm-none d-xs-none">
-            Lorem ipsum dolor sit amet consectetur adipiscing
+            Unlock your writing potential and connect with the creative economy
             <br />
           </h2>
           <h3 className=" d-xs-block d-sm-block d-md-block d-lg-none">
-            Lorem ipsum dolor sit amet consectetur adipiscing
+            Unlock your writing potential and connect with the creative economy
             <br />
           </h3>
-          <p className="mt-4">
-            Lorem ipsum dolor sit amet consectetur adipiscing lorem ipsum dolor
-            sit amet consectetur adipiscing
+          <p className="mt-2">
+            Your Ultimate Writing Companion for Book Writing, Launching, and
+            Selling. Explore our Creative Marketplace for Authors and MSMEs!
           </p>
-          <div className="d-none d-lg-block d-md-block d-sm-none d-xs-none mt-4">
+          <div className="d-none d-lg-block d-md-block d-sm-none d-xs-none mt-2">
             <CreativesLink /> <AuthorsLink />
           </div>
           <div className="d-none d-lg-block d-md-block d-sm-none d-xs-none mt-4">
             <WorkWithUsBtn />
+            <ChallengeBtn />
           </div>
         </div>
         <div className="col-lg-5 col-md-5 col-sm-12">
@@ -43,6 +45,7 @@ export const SimplePage = () => {
           </div>
           <div className="d-xs-block d-sm-block d-md-none d-lg-none mt-4 text-center">
             <WorkWithUsBtn />
+            <ChallengeBtn />
           </div>
         </div>
       </div>
@@ -63,24 +66,24 @@ export const SimplePage = () => {
         </div>{" "}
         <div className="d-flex justify-content-center">
           <a
-            href="tel:2348056009272"
+            href="tel:2349065996599"
             className="text-decoration-none text-black"
           >
             <p className="paragraphColor mx-3 textShow m-0 p-1">
-              +2348056009272
+              +2349065996599
             </p>
           </a>
         </div>{" "}
         <div className="d-flex justify-content-center">
           <a
-            href=""
+            href="https://www.linkedin.com/company/joleving/"
             target="_blank"
             className="text-decoration-none text-black my-2"
           >
             <i class="bi bi-linkedin mx-2 paragraphColor textShow"></i>
           </a>
           <a
-            href=""
+            href="https://instagram.com/joleving?igshid=MzRlODBiNWFlZA=="
             target="_blank"
             className="text-decoration-none text-black my-2"
           >
