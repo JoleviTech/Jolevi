63071c86a741b1892f2726da5e90b309c3eb48ec
opeyemibioku <opeyemibioku@gmail.com>
2023-08-26 19:25:24 +0100

    updated the contact and blog

--- src/components/website/cards/blogCards.js
+++ src/components/website/cards/blogCards.js
@@ -1,23 +1,25 @@
-// //BlogPostCard
-// //RelatedPosts
-// import React from "react";
-// import "../../website/cards/blogCards.css";
+//BlogPostCard
+//RelatedPosts
+import React from "react";
 
-// function blogCards({ imageUrl, title, description }) {
-//   return (
-//     <div>
-//       <div className="card-container mb-3">
-//         <img id="headerimg" src={imageUrl} alt="" />
-//         <div className="card-content">
-//           <p id="header">LOREM ISPUM</p>
-//           <p id="desc">Lorem ipsum dolor sit amet <br/>consectetur sit</p>
-//         </div>
-//         <div className="d-flex justify-content-end">
-//           <a>Read More </a>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
+function blogCards({ imageUrl, title, description }) {
+  return (
+    <div>
+      <div className="card-container mb-3">
+        <img id="headerimg" src={imageUrl} alt="" />
+        <div className="card-content">
+          <p id="header">LOREM ISPUM</p>
+          <p id="desc">
+            Lorem ipsum dolor sit amet <br />
+            consectetur sit
+          </p>
+        </div>
+        <div className="d-flex justify-content-end blog-read-more-link">
+          <a>Read More </a>
+        </div>
+      </div>
+    </div>
+  );
+}
 
-// export default blogCards;
+export default blogCards;
