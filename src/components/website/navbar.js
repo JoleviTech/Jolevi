b8a968e707a5cd4f6c7998c2ba28cb332716064d
opeyemibioku <opeyemibioku@gmail.com>
2023-08-26 18:13:46 +0100

    updates

--- src/components/website/navbar.js
+++ src/components/website/navbar.js
@@ -209,36 +209,36 @@ export const NewNavbarr = () => {
     </nav>
   );
 };
-// export const Navbar = () => {
-//   return (
-//     <nav className="navbar navbar-expand-lg navbar-light">
-//       <div className="container-fluid px-2 py-2">
-//         <Link to="/" className="navbar-brand ms-lg-5">
-//           {" "}
-//           <img
-//             src={jolevilogo}
-//             className="img-fluid"
-//             alt="jolevi"
-//             width="120"
-//             height="124"
-//           />
-//         </Link>
-//         <button
-//           className="navbar-toggler btn"
-//           type=""
-//           data-bs-toggle="collapse"
-//           data-bs-target="#navbarNavDarkDropdown"
-//           aria-controls="navbarNavDarkDropdown"
-//           aria-expanded="false"
-//           aria-label="Toggle navigation"
-//         >
-//           <span className="navbar-toggler-icon"></span>
-//         </button>
-//         <div
-//           className="collapse navbar-collapse justify-content-end"
-//           id="navbarNavDarkDropdown"
-//         ></div>
-//       </div>
-//     </nav>
-//   );
-// };
+export const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light">
+      <div className="container-fluid px-2 py-2">
+        <Link to="/" className="navbar-brand ms-lg-5">
+          {" "}
+          <img
+            src={jolevilogo}
+            className="img-fluid"
+            alt="jolevi"
+            width="120"
+            height="124"
+          />
+        </Link>
+        <button
+          className="navbar-toggler btn"
+          type=""
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavDarkDropdown"
+          aria-controls="navbarNavDarkDropdown"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div
+          className="collapse navbar-collapse justify-content-end"
+          id="navbarNavDarkDropdown"
+        ></div>
+      </div>
+    </nav>
+  );
+};
